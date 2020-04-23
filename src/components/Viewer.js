c65371a94cfde5a5de9f1321fa84d0b44d1b496e
mohitrajane <mhit98@gmail.com>
2020-04-24 03:00:12 +0530

    Add basic styling for Profile and Work Experience

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -14,34 +14,44 @@ function Viewer(){
 
     return(
         <div className="min-vh-100 w-100">
-            <div style={a4Style} className="shadow mx-auto my-5">
-            <div>
-                profile: <p>{profile.firstName}</p>
-                lastNAme: <p>{profile.lastName}</p>
-                addredd1 : <p>{profile.addressLine1}</p>
-                addredd2 : <p>{profile.addressLine2}</p>
-                addredd3 : <p>{profile.addressLine3}</p>
-                phoneno: <p>{profile.phone}</p>
-                email : <p>{profile.email}</p>
-
+            <div style={a4Style} className="shadow mx-auto my-5 p-5">
+            <div id="profile" className="d-flex">
+                <div className="w-100">
+                    <h1>{profile.firstName+' '+profile.lastName}</h1>
+                    <div className="d-flex flex-column">
+                        <span class="font-weight-light">{profile.addressLine1}</span>
+                        <span class="font-weight-light">{profile.addressLine2}</span>
+                        <span class="font-weight-light">{profile.addressLine3}</span>
+                    </div>
+                </div>
+                <div className="flex-shrink-1 my-auto">
+                    <p>{profile.phone}</p>
+                    <p>{profile.email}</p>
+                </div>
             </div>
-            <div>
+            <hr/>
+            <h6 className="p-2">Work Experience</h6>
+            <div id="experience" className="d-flex flex-column">
+                {experience.map((item)=>(
+                    <div className="d-flex w-100 p-4">
+                        <div className="w-100" key={item.id}>
+                            <h5 className="mb-0">{item.company}</h5>
+                            <span className="font-weight-light" style={{fontSize:'0.75rem'}}>{item.designation}</span>
+                        </div>
+                        <div className="flex-shrink-0">
+                            <p className="font-weight-light" style={{fontSize:'0.75rem'}}>{item.start} - {item.end}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div id="education" className="d-flex">
+
                 {education.map((item)=>(
                     <p key={item.id}>
                         name:{item.institution}
                         start:{item.start}
                         end:{item.end}
                         degree:{item.degree}
-                    </p>
-                ))}
-            </div>
-            <div>
-                {experience.map((item)=>(
-                    <p key={item.id}>
-                        name:{item.company}
-                        start:{item.start}
-                        end:{item.end}
-                        degree:{item.destination}
                     </p>
                 ))}
             </div>
