4c7cd6a15aaa5449a40ab95b841dd2e147aad7b5
mohitrajane <mhit98@gmail.com>
2020-04-24 03:28:11 +0530

    Styleing improvment's to Viewer and small bug fix in Profile

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -15,54 +15,59 @@ function Viewer(){
     return(
         <div className="min-vh-100 w-100">
             <div style={a4Style} className="shadow mx-auto my-5 p-5">
-            <div id="profile" className="d-flex">
-                <div className="w-100">
-                    <h1>{profile.firstName+' '+profile.lastName}</h1>
-                    <div className="d-flex flex-column">
-                        <span class="font-weight-light">{profile.addressLine1}</span>
-                        <span class="font-weight-light">{profile.addressLine2}</span>
-                        <span class="font-weight-light">{profile.addressLine3}</span>
+                <div id="profile" className="d-flex">
+                    <div className="w-100">
+                        <h1>{profile.firstName+' '+profile.lastName}</h1>
+                        <div className="d-flex flex-column">
+                            <span class="font-weight-light">{profile.addressLine1}</span>
+                            <span class="font-weight-light">{profile.addressLine2}</span>
+                            <span class="font-weight-light">{profile.addressLine3}</span>
+                        </div>
+                    </div>
+                    <div className="flex-shrink-1 my-auto">
+                        <p>{profile.phone}</p>
+                        <p>{profile.email}</p>
                     </div>
                 </div>
-                <div className="flex-shrink-1 my-auto">
-                    <p>{profile.phone}</p>
-                    <p>{profile.email}</p>
-                </div>
-            </div>
-            <hr/>
-            <h6 className="p-2">Work Experience</h6>
-            <div id="experience" className="d-flex flex-column">
-                {experience.map((item)=>(
-                    <div className="d-flex w-100 p-4">
-                        <div className="w-100" key={item.id}>
-                            <h5 className="mb-0">{item.company}</h5>
-                            <span className="font-weight-light" style={{fontSize:'0.75rem'}}>{item.designation}</span>
+                <hr/>
+                <h6 className="p-2">Work Experience</h6>
+                <div id="experience" className="d-flex flex-column">
+                    {experience.map((item)=>(
+                        <div className="d-flex w-100 p-4">
+                            <div className="w-100" key={item.id}>
+                                <h5 className="mb-0">{item.company}</h5>
+                                <span className="font-weight-light" style={{fontSize:'0.75rem'}}>{item.designation}</span>
+                            </div>
+                            <div className="flex-shrink-0">
+                                <p className="font-weight-light" style={{fontSize:'0.75rem'}}>{item.start} - {item.end}</p>
+                            </div>
                         </div>
-                        <div className="flex-shrink-0">
+                    ))}
+                </div>
+                <hr/>
+                <h6 className="p-2">Education</h6>
+                <div id="education" className="d-flex flex-column">
+                    {education.map((item)=>(
+                        <div className="d-flex w-100 p-4">
+                            <div className="w-100" key={item.id}>
+                            <h5 className="mb-0">{item.institution}</h5>
+                            <span className="font-weight-light" style={{fontSize:'0.75rem'}}>{item.degree}</span>
+                            </div>
+                            <div className="flex-shrink-0">
                             <p className="font-weight-light" style={{fontSize:'0.75rem'}}>{item.start} - {item.end}</p>
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-            <div id="education" className="d-flex">
-
-                {education.map((item)=>(
-                    <p key={item.id}>
-                        name:{item.institution}
-                        start:{item.start}
-                        end:{item.end}
-                        degree:{item.degree}
-                    </p>
-                ))}
-            </div>
-            <div>
-                {skills.map((item)=>(
-                    <p>
-                        {item}
-                    </p>
-                    
-                ))}
-            </div>
+                    ))}
+                </div>
+                <hr/>
+                <h6 className="p-2">Skills</h6>
+                <div id="skills" className="d-flex">
+                    {skills.map((item)=>(
+                        <p className="badge badge-primary mx-2">
+                            {item}
+                        </p>
+                    ))}
+                </div>
             </div>
         </div>
     )
--- src/components/tabs/Profile.js
+++ src/components/tabs/Profile.js
@@ -1,46 +1,47 @@
 import React from 'react';
-import { useDispatch} from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 import {updateProfile} from '../../action';
 import Skills from "./Skills";
 
 const Profile = () =>{
     const dispatch = useDispatch();
+    const profile = useSelector(state => state.profile);
     return(
         <div> {/*className="min-vh-100 position-fixed"*/}
                 <div className="d-flex  col-lg-10 mx-auto flex-column">
                         <div className="my-2 d-flex flex-column">
                             <span>First name</span>
-                            <input type="text" placeholder="Jane" onChange={(e) => dispatch(updateProfile(e.target.value,'firstName'))} ></input>
+                            <input type="text" placeholder="Jane" value={profile.firstName?profile.firstName:undefined} onChange={(e) => dispatch(updateProfile(e.target.value,'firstName'))} ></input>
                         </div>
                         <div className="my-2 d-flex flex-column">
                             <span>Last name</span>
-                            <input type="text" placeholder="Doe" onChange={(e) => dispatch(updateProfile(e.target.value,'lastName'))}></input>
+                            <input type="text" placeholder="Doe" value={profile.lastName?profile.lastName:undefined} onChange={(e) => dispatch(updateProfile(e.target.value,'lastName'))}></input>
                         </div>
                 </div>
                 <hr/>
                 <div className="d-flex col-lg-10 mx-auto flex-column">
                     <div className="my-2 d-flex flex-column">
                         <span>Address Line 1</span>
-                        <input type="text" placeholder="House no" onChange={(e) => dispatch(updateProfile(e.target.value,'addressLine1'))}></input>
+                        <input type="text" placeholder="House no" value={profile.addressLine1?profile.addressLine1:undefined} onChange={(e) => dispatch(updateProfile(e.target.value,'addressLine1'))}></input>
                         </div>
                     <div className="my-2 d-flex flex-column">
                         <span>Address Line 2</span>
-                        <input type="text" placeholder="Lane/location" onChange={(e) => dispatch(updateProfile(e.target.value,'addressLine2'))}></input>
+                        <input type="text" placeholder="Lane/location" value={profile.addressLine2?profile.addressLine2:undefined} onChange={(e) => dispatch(updateProfile(e.target.value,'addressLine2'))}></input>
                     </div>
                     <div className="my-2 d-flex flex-column">
                         <span>Address Line 3</span>
-                        <input type="text" placeholder="City" onChange={(e) => dispatch(updateProfile(e.target.value,'addressLine3'))}></input>
+                        <input type="text" placeholder="City" value={profile.addressLine3?profile.addressLine3:undefined} onChange={(e) => dispatch(updateProfile(e.target.value,'addressLine3'))}></input>
                     </div>
                 </div>
                 <hr/>
                 <div className="d-flex col-lg-10 mx-auto flex-column">
                     <div className="my-2 d-flex flex-column">
                         <span>Phone Number</span>
-                        <input type="text" onChange={(e) => dispatch(updateProfile(e.target.value,'phone'))}></input>
+                        <input type="text" placeholder="+91 xxxx xxx xxx" value={profile.phone?profile.phone:undefined}  onChange={(e) => dispatch(updateProfile(e.target.value,'phone'))}></input>
                         </div>
                     <div className="my-2 d-flex flex-column">
                         <span>Email address</span>
-                        <input type="text" onChange={(e) => dispatch(updateProfile(e.target.value,'email'))}></input>
+                        <input type="text" placeholder="jane.doe@provider.com" value={profile.email?profile.email:undefined}  onChange={(e) => dispatch(updateProfile(e.target.value,'email'))}></input>
                     </div>
                 </div>
                 <hr/>
