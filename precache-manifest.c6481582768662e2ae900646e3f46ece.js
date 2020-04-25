cd293add9f1bd30212f6a57e5cfd2a6e66475d4b
mohitrajane <mhit98@gmail.com>
2020-04-25 12:46:47 +0530

    Updates

---
+++ precache-manifest.c6481582768662e2ae900646e3f46ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a27f1f96e34b893a1e385bdfb1b1e48",
+    "url": "/resume_builder/index.html"
+  },
+  {
+    "revision": "9012826338999a1f7d07",
+    "url": "/resume_builder/static/css/2.f6fed5cb.chunk.css"
+  },
+  {
+    "revision": "9012826338999a1f7d07",
+    "url": "/resume_builder/static/js/2.2d0d9427.chunk.js"
+  },
+  {
+    "revision": "e3637ec2c8676e842f1853e1877f9e14",
+    "url": "/resume_builder/static/js/2.2d0d9427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29534bf964062a11302",
+    "url": "/resume_builder/static/js/main.35b1ecb4.chunk.js"
+  },
+  {
+    "revision": "332e66b350b4477cf8a5",
+    "url": "/resume_builder/static/js/runtime-main.ee898201.js"
+  }
+]);
